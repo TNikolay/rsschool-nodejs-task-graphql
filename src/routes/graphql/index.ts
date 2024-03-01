85e66ae623dbb9a90a89fb1c49ece29775931baa
Nikolay Tyushkov <begemot@begemotov.net>
2024-03-01 13:32:08 +0300

    feat: something works

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,20 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import {
+  GraphQLEnumType,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  graphql,
+} from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -12,8 +24,34 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
+
     async handler(req) {
-      return {};
+      const source = req.body.query;
+      console.log({ source });
+
+      const query = new GraphQLObjectType({
+        name: 'query',
+        fields: {
+          posts: {
+            type: new GraphQLList(GraphQLString),
+            resolve: async () => {
+              return (await prisma.post.findMany()).map((post) => post.title);
+            },
+          },
+        },
+      });
+
+      const schema = new GraphQLSchema({ query });
+
+      const res = await graphql({
+        schema,
+        source,
+        contextValue: {
+          prisma,
+        },
+      });
+
+      return { data: res.data, errors: res.errors };
     },
   });
 };
